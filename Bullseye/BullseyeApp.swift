67c6ec51b89d9a3f3487cd244c2d7004f5644bc7
yujung <yujung.son@onekey.kr>
2021-09-09 15:22:51 +0900

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by yujung on 2021/09/09.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
