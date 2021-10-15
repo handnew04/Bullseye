3bb9b96b84ffb0c954a0d0c8b2985f233749da0c
yujung <yujung.son@onekey.kr>
2021-10-16 01:27:13 +0900

    feat: 버튼 디자인 변경

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,37 +14,51 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("⚾️⚾️⚾️\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            HStack {
-                Text("1")
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("⚾️⚾️⚾️\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
                     .bold()
-                Slider(value: $sliderValue, in:1.0...100.0)
-                Text("100")
-                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .padding(.leading, 30.0)
+                    .padding(.trailing, 30.0)
+                    .colorMultiply(Color("TextViewColor"))
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $sliderValue, in:1.0...100.0)
+                    Text("100")
+                        .bold()
+                }
+                .padding()
+                Button(action: {
+                    print("Hello, SwiftUI!")
+                    alertIsVisible = true
+                }) {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding(20.0)
+                .background(ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                })
+                .foregroundColor(Color.white)
+                .cornerRadius(21.0)
+                .alert(isPresented: $alertIsVisible, content: {
+                    let roundedValue = Int(sliderValue.rounded())
+                    return Alert(title: Text("Hello there!"), message: Text("slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+                })
             }
-            Button(action: {
-                print("Hello, SwiftUI!")
-                alertIsVisible = true
-            }) {
-                Text("Hit me")
-            }
-            .background(Color.blue)
-            .foregroundColor(Color.white)
-            .padding(20.0)
-            .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue = Int(sliderValue.rounded())
-                return Alert(title: Text("Hello there!"), message: Text("slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
-            })
         }
     }
 }
@@ -52,8 +66,13 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 586, height: 320))
+        ContentView()
+            .preferredColorScheme(.dark)
             .environment(\.sizeCategory, .large)
         ContentView()
+            .preferredColorScheme(.dark)
             .previewLayout(.fixed(width: 586, height: 320))
     }
 }
