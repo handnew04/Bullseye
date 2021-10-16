7fbebff3ff686a279b00ac9efec61875181facca
yujung <handnew04@gmail.com>
2021-10-16 17:24:53 +0900

    feat: textview.swift 파일 생성(커밋테스트)

---
+++ Bullseye/Views/TextViews.swift
@@ -0,0 +1,20 @@
+//
+//  TextViews.swift
+//  Bullseye
+//
+//  Created by yujung on 2021/10/16.
+//
+
+import SwiftUI
+
+struct TextViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextViews_Previews: PreviewProvider {
+    static var previews: some View {
+        TextViews()
+    }
+}
